e5670bdf415cfabe453aade00556d51d1a38de74
Anatolii Zenin <anatolii.zenin@gmail.com>
2023-09-14 11:23:48 +0200

    add try-catch wrapper to avoid crashes and display errors. use enhanced switch

--- module-main/src/main/java/com/mjc/school/Main.java
+++ module-main/src/main/java/com/mjc/school/Main.java
@@ -1,8 +1,7 @@
 package com.mjc.school;
 
 import com.mjc.school.controller.Controller;
-import com.mjc.school.controller.CController;
-import com.mjc.school.dto.NewsDTO;
+import com.mjc.school.controller.implementation.CController;
 
 import java.util.Scanner;
 
@@ -14,35 +13,27 @@ public class Main {
         Scanner in = new Scanner(System.in);
         boolean exit = false;
         while (!exit) {
-            System.out.println("Select operation:\n" +
-                    "0. Get all news\n" +
-                    "1. Get news by ID\n" +
-                    "2. Create news\n" +
-                    "3. Update news\n" +
-                    "4. Delete news by ID\n" +
-                    "-1. Exit");
-            int operationChoice = Integer.parseInt(in.nextLine());
-            switch (operationChoice) {
-                case 0:
-                    NewsMethods.getAllNews(newsController);
-                    break;
-                case 1:
-                    NewsMethods.getNewsById(newsController);
-                    break;
-                case 2:
-                    NewsMethods.createNews(newsController);
-                    break;
-                case 3:
-                    NewsMethods.updateNews(newsController);
-                    break;
-                case 4:
-                    NewsMethods.deleteNewsById(newsController);
-                    break;
-                case -1:
-                    exit = true;
-                    break;
-                default:
-                    System.out.println("No such operation: " + operationChoice);
+            try {
+                System.out.println("""
+                        Select operation:
+                        0. Get all news
+                        1. Get news by ID
+                        2. Create news
+                        3. Update news
+                        4. Delete news by ID
+                        -1. Exit""");
+                int operationChoice = Integer.parseInt(in.nextLine());
+                switch (operationChoice) {
+                    case 0 -> NewsMethods.getAllNews(newsController);
+                    case 1 -> NewsMethods.getNewsById(newsController);
+                    case 2 -> NewsMethods.createNews(newsController);
+                    case 3 -> NewsMethods.updateNews(newsController);
+                    case 4 -> NewsMethods.deleteNewsById(newsController);
+                    case -1 -> exit = true;
+                    default -> System.out.println("No such operation: " + operationChoice);
+                }
+            } catch (RuntimeException e) {
+                System.out.println(e.getMessage());
             }
         }
     }
